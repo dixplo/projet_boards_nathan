25b29b7c15f4050cebbe6a595a8eb960c8f8aa0f
Nathan PRIOU <priounathan@gmail.com>
2020-04-09 12:04:15 +0200

    modif sur story

--- app/routes/story/new.js
+++ app/routes/story/new.js
@@ -1,13 +1,17 @@
 import Route from '@ember/routing/route';
 import {set} from '@ember/object'
+import RSVP from 'rsvp'
 
 export default Route.extend({
   model(params) {
-    return this.get('store').peekRecord('project', params.project_id);
+    return RSVP.hash({
+      project: this.get('store').peekRecord('project', params.project_id),
+      developers: this.get('store').findAll('developer')
+    });
   },
   actions:{
-    addNew(code,description, project){
-      let story = this.get('store').createRecord('story',{code:code,description:description});
+    addNew(code,description,developer,project){
+      let story = this.get('store').createRecord('story',{code:code,description:description,developer:developer});
       set(story,'project',project);
       story.save().then(()=>{
         project.save().then(()=>{this.transitionTo('projects');});
